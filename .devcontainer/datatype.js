5c19500865ef031b3c4ad5e2a09f4ab6f8558359
Neetu <163496961+neetuweb123@users.noreply.github.com>
2024-12-25 11:09:55 +0000

    container config done

---
+++ .devcontainer/datatype.js
@@ -0,0 +1,14 @@
+// alert(2+2)
+let name ="neetu"
+let age = 22
+let isLoggedIn = false
+
+// number => to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined 
+// Symbol => uniqe
+// Object
+console.log(typeof undefined)
--- .devcontainer/variable.js
+++ .devcontainer/variable.js
@@ -2,12 +2,17 @@ const account_Id = 123455
 let accountEmail = "neetu977@gmail.com"
 var accountPassword = "12345"
 accountCity = "Delhi"
+let accountState;
 
-// account_Id = 2
+// account_Id = 2 not allowed
 accountEmail = "nas@,com"
 accountPassword = "56789"
 accountCity = "japan"
 console.log(account_Id);
-console.table([accountEmail, accountPassword, accountCity])
+console.table([accountEmail, accountPassword, accountCity, accountState])
 
-{ is called scop}
+// { is called scope}
+/*
+Prefer not  to use var
+because of issue in block scope and function scope
+*/
