08630d01023c6f338a6be721f71d32489b0c732a
Neetu <163496961+neetuweb123@users.noreply.github.com>
2024-12-25 11:34:48 +0000

    conversion operationdone

---
+++ .devcontainer/conversionOperation.js
@@ -0,0 +1,13 @@
+let score = "22"
+console.log(typeof score);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+let isLoggedIn = ""
+ let BooleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(BooleanIsLoggedIn);
+ let someNumber = 22
+ let StringNumber = String(someNumber)
+ console.log(StringNumber);
+ console.log(typeof StringNumber)
