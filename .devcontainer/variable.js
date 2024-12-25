819cab82b821f87d8aeab07ed9140e754fbc300e
Neetu <163496961+neetuweb123@users.noreply.github.com>
2024-12-25 10:32:18 +0000

    container config done

---
+++ .devcontainer/variable.js
@@ -0,0 +1,11 @@
+const account_Id = 123455
+let accountEmail = "neetu977@gmail.com"
+var accountPassword = "12345"
+accountCity = "Delhi"
+
+// account_Id = 2
+accountEmail = "nas@,com"
+accountPassword = "56789"
+accountCity = "japan"
+console.log(account_Id);
+console.table([accountEmail, accountPassword, accountCity])
