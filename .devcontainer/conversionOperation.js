48a9b1b30c0f1eb3a6b274f20518cd745c17baff
Neetu <163496961+neetuweb123@users.noreply.github.com>
2024-12-26 16:14:37 +0000

    conversion config done

--- .devcontainer/conversionOperation.js
+++ .devcontainer/conversionOperation.js
@@ -11,3 +11,20 @@ let isLoggedIn = ""
  let StringNumber = String(someNumber)
  console.log(StringNumber);
  console.log(typeof StringNumber)
+// node conversionOperation.js
+//  ***************************operation*********************
+let value = 3
+let negValue = -value
+let str1 = "hello"
+let str2 = "  neetu"
+
+let str3 = str1 + str2
+console.log(str3)
+console.log("1" + 2);
+console.log(1 + "2");
+console.log(1 + 2 + "3")
+let num1, num2, num3
+
+let gameCounter = 100
+gameCounter;
+console.log(gameCounter)
